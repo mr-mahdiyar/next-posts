bf84a743c8954da3c066d4046dc5a78d18323eda
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-09 09:49:26 +0330

    add and reset buttons client side component was created.

---
+++ components/form-submit.js
@@ -0,0 +1,15 @@
+"use client";
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmit() {
+  const status = useFormStatus();
+if(status.pending) {
+    return  <p>creating post...</p>
+}
+  return (
+    <>
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
+    </>
+  );
+}
