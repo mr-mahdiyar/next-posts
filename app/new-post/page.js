31265106f2da2c1608960c02abad80c4a54a6b84
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-09 14:41:53 +0330

    form validation was added.

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,10 +1,11 @@
-import FormSubmit from '@/components/form-submit';
+
+import PostForm from '@/components/post-component';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 
 export default function NewPostPage() {
 
-async function createPost(formData) { //define a form action. It have to be async.
+async function createPost(prevState, formData) { // define a form action. It has to be async.
     "use server" // convert form action to server action
     const title = formData.get("title");
     const image = formData.get("image");
@@ -17,34 +18,21 @@ async function createPost(formData) { //define a form action. It have to be asyn
       userId: 1
     })
 
+    let errors = [];
+    if(!title || title.trim().length === 0) {
+      errors.push("Title is required!...")
+    }
+    if(!content || content.trim().length === 0) {
+      errors.push("content is required!...")
+    }
+    if(!image) {
+      errors.push("Image is required.")
+    }
+    if(errors.length > 0) {
+      return {errors}
+    }
     redirect("/feed")
   }
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  
+    return <PostForm action={createPost} />
 }
---
+++ components/post-component.js
@@ -0,0 +1,43 @@
+"use client";
+import FormSubmit from "@/components/form-submit";
+import { useFormState } from "react-dom";
+
+export default function PostForm({ action }) {
+  // first output includes inputs data, error or success messages and second output is a potinter to a function that sends form datas to server, update form state and somethings like them.
+  const [state, formAction] = useFormState(action, {});
+  // first argument is a function that sends request to API for send form data. second is initial state.
+  return (
+    <>
+      <h1>Create a new post</h1>
+      <form action={formAction}>
+        <p className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" />
+        </p>
+        <p className="form-control">
+          <label htmlFor="image">Image URL</label>
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            id="image"
+            name="image"
+          />
+        </p>
+        <p className="form-control">
+          <label htmlFor="content">Content</label>
+          <textarea id="content" name="content" rows="5" />
+        </p>
+        <p className="form-actions">
+          <FormSubmit />
+        </p>
+        {state.errors && (
+          <ul className="form-errors">
+            {state.errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+      </form>
+    </>
+  );
+}
