f01bc4fbe3b08436b7990f5d2c3c56d287c9db04
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-11 09:37:02 +0330

    save images to cloadinary mechanism was added.

--- actions/posts.js
+++ actions/posts.js
@@ -1,34 +1,41 @@
-"use server"
+"use server";
 
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
+import { uploadImage } from "@/lib/cloudinary";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
 
-export async function createPost(prevState, formData) { // define a form action. It has to be async.
-    //"use server" // convert form action to server action
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
+export async function createPost(prevState, formData) {
+  // define a form action. It has to be async.
+  //"use server" // convert form action to server action
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
 
-    let errors = [];
-    if(!title || title.trim().length === 0) {
-      errors.push("Title is required!...")
-    }
-    if(!content || content.trim().length === 0) {
-      errors.push("content is required!...")
-    }
-    if(!image) {
-      errors.push("Image is required.")
-    }
-    if(errors.length > 0) {
-      // form action must return a something taht represent form state.
-      return {errors}
-    }
-    await storePost({
-        imageUrl: '',
-        title,
-        content,
-        userId: 1
-      })
-    redirect("/feed")
+  let errors = [];
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required!...");
   }
-  
+  if (!content || content.trim().length === 0) {
+    errors.push("content is required!...");
+  }
+  if (!image) {
+    errors.push("Image is required.");
+  }
+  if (errors.length > 0) {
+    // form action must return a something taht represent form state.
+    return { errors };
+  }
+  let imageURL;
+  try {
+   imageURL = await uploadImage(image);
+  } catch (error) {
+    throw new Error("Image upload failed, post was not created. Please try again later.")
+  }
+  await storePost({
+    imageUrl: imageURL,
+    title,
+    content,
+    userId: 1,
+  });
+  redirect("/feed");
+}
