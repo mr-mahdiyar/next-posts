1aa74706cbd1f80e799d38d67aeb2b0250318a6e
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-11 12:11:26 +0330

    UX was improved. changes can manage with useOptimistic.

--- components/posts.js
+++ components/posts.js
@@ -1,8 +1,10 @@
+"use client"
+import { useOptimistic } from "react";
 import { formatDate } from "@/lib/format";
 import LikeButton from "./like-icon";
 import { togglePostLikeStatus } from "@/actions/posts";
 
-function Post({ post }) {
+function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
@@ -20,7 +22,10 @@ function Post({ post }) {
             </p>
           </div>
           <div>
-            <form action={togglePostLikeStatus.bind(null, post.id)} className={post.isLiked && "liked"}>
+            <form
+              action={action.bind(null, post.id)}
+              className={post.isLiked && "liked"}
+            >
               <LikeButton />
             </form>
           </div>
@@ -32,15 +37,53 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+
+    // optimisticPosts: current posts state.
+    // updatedOptimisticPosts: a function for update the posts based changes and user actions.
+
+    posts, // array of posts. specify initialize posts status.
+
+    //  different between posts and prevPosts: posts comes from props; in other word from another component
+    //  and can change in any time from out of this component.
+    //   but prevPosts uses in this function and help us to manage what do we wanna to do. 
+
+    (prevPosts, updatedPostId) => {
+
+    // prevPosts: array of posts that have to update.
+    // updatedPostId: id of post that have to update. this value come from argument that pass to updateOptimisticePosts. 
+
+      const updatedPostIndex = prevPosts.findIndex(
+        (post) => post.id === updatedPostId
+      );
+      if (updatedPostIndex == -1) {
+        return prevPosts;
+      }
+
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+
+      return newPosts;
+    }
+  );
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
   }
 
+  async function updatePost(postId) {
+
+    updateOptimisticPosts(postId)
+    await togglePostLikeStatus(postId)
+  }
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatePost} />
         </li>
       ))}
     </ul>
