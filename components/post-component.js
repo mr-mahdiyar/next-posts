dbe05c834f74c0168b8c64311311525b075cf02b
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-11 09:35:41 +0330

    title of image url input was editted.

--- components/post-component.js
+++ components/post-component.js
@@ -16,7 +16,7 @@ export default function PostForm({ action }) {
           <input type="text" id="title" name="title" />
         </p>
         <p className="form-control">
-          <label htmlFor="image">Image URL</label>
+          <label htmlFor="image">Image</label>
           <input
             type="file"
             accept="image/png, image/jpeg"
