ad3c68d4705235058dffdc0093197493476ecc70
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-11 11:02:25 +0330

    button was repped by form and relevant server action for that was added.

--- components/posts.js
+++ components/posts.js
@@ -1,5 +1,6 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostLikeStatus } from "@/actions/posts";
 
 function Post({ post }) {
   return (
@@ -12,14 +13,16 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
           <div>
-            <LikeButton />
+            <form action={togglePostLikeStatus.bind(null, post.id)} className={post.isLiked && "liked"}>
+              <LikeButton />
+            </form>
           </div>
         </header>
         <p>{post.content}</p>
