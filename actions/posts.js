c503c51e6dcf4f4314d7c694564e9f322f1c0c0b
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-11 11:00:06 +0330

    server function for processing the like button click was added.

--- actions/posts.js
+++ actions/posts.js
@@ -1,7 +1,8 @@
 "use server";
 
 import { uploadImage } from "@/lib/cloudinary";
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createPost(prevState, formData) {
@@ -39,3 +40,8 @@ export async function createPost(prevState, formData) {
   });
   redirect("/feed");
 }
+
+export async function togglePostLikeStatus (postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/feed")
+};
